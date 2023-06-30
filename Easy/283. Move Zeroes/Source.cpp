5bcc7e9df49d1916cfed17452b328b8c3b448bcb
Yousef <yousef.wael2002@gmail.com>
2023-06-30 22:46:16 +0300

    283. Move Zeroes

--- Easy/283. Move Zeroes/Source.cpp
+++ Easy/283. Move Zeroes/Source.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+
 using namespace std;
 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+		queue<int> zeroQueue;
+		queue<int> nonZeroQueue;
+
 		for (int i = 0; i < nums.size(); i++)
 		{
-			int temp;
-			while (nums[i] == 0)
+			if (nums[i] == 0)
 			{
-				temp = nums[i];
-				nums.erase(nums.begin() + i);
-				nums.push_back(temp);
+				zeroQueue.push(nums[i]);
+			}
+			else
+			{
+				nonZeroQueue.push(nums[i]);
+			}
+		}
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (!nonZeroQueue.empty())
+			{
+				nums[i] = nonZeroQueue.front();
+				nonZeroQueue.pop();
+			}
+			else
+			{
+				nums[i] = zeroQueue.front();
+				zeroQueue.pop();
 			}
 		}
     }
@@ -21,7 +41,7 @@ public:
 int main()
 {
 	Solution test1;
-	vector<int> result1 = {0,0,0,1};
+	vector<int> result1 = {1,4,3,5,2,0};
 	test1.moveZeroes(result1);
 	for (int i = 0; i < result1.size(); i++)
 	{
