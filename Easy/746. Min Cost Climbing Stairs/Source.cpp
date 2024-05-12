20f1a8c2c356a31bc838c1aebd8c741a58692b44
Yousef Wael <yousef.wael2002@gmail.com>
2024-05-12 12:12:02 +0300

    746. Min Cost Climbing Stairs

--- Easy/746. Min Cost Climbing Stairs/Source.cpp
+++ Easy/746. Min Cost Climbing Stairs/Source.cpp
@@ -5,21 +5,18 @@ using namespace std;
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int minCost = 0;
-        for (int i = 0; i < cost.size(); i++)
+        cost.push_back(0);
+        for (int i = cost.size() - 4; i >= 0; i--)
         {
-            if ((cost[i] < cost[i+1]) && (i < cost.size()))
-            {
-                minCost += cost[i];
-            }
+            cost[i] += min(cost[i + 1], cost[i + 2]);
         }
-        return minCost;
+        return min(cost[0], cost[1]);
     }
 };
 
 int main() {
     Solution test;
-    vector<int> arr = { 10,15,20 };
+    vector<int> arr = { 1,100,1,1,1,100,1,1,100,1 };
     cout << test.minCostClimbingStairs(arr);
     return 0;
 }
