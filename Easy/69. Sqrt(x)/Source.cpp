e1edea96165952904966b05fcecf9a2589b3ba0f
Yousef Wael <yousef.wael2002@gmail.com>
2024-05-31 22:06:12 +0300

    69. Sqrt(x)

--- Easy/69. Sqrt(x)/Source.cpp
+++ Easy/69. Sqrt(x)/Source.cpp
@@ -5,16 +5,31 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-        int ans;
-        // Newton raphson method with one iteration
-        ans = 0.5 * ((x - 1) + x / (x - 1));
-        if (x / 4 == ans) {
-            return ans;
-        }
-        else
+        int l = 0, r = x / 2, ans;
+        long long mid;
+        if (x == 0 || x == 1)
         {
-            ans = mySqrt(ans);
+            return x;
         }
+
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+            if (mid * mid == x)
+            {
+                return mid;
+            }
+
+            if (mid * mid < x)
+            {
+                l = mid + 1;
+                ans = mid;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return ans;
     }
 };
 
