330f29e7b57502035b5aea5b4dad833f4ccafea2
Yousef Wael <yousef.wael2002@gmail.com>
2024-05-31 23:09:04 +0300

    50. Pow(x, n) (Unfinished)

--- Medium/50. Pow(x, n)/Source.cpp
+++ Medium/50. Pow(x, n)/Source.cpp
@@ -1,15 +1,48 @@
 #include <iostream>;
+#include <math.h>;
 using namespace std;
 
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        if (n == 0 || x == 1)
+        {
+            return 1;
+        }
+        if (n == 1)
+        {
+            return x;
+        }
+
+        if (x == -1 && n%2==0)
+        {
+            return 1;
+        }
+
+        if (x == -1 && n % 2 != 0)
+        {
+            return -1;
+        }
+
+        int power = abs(n);
+        double result = x;
+        for (int i = 1; i < power; i++)
+        {
+            result *= x;
+        }
+
+        if (n > 0)
+        {
+            return result;
+        }
+        else {
+            return 1 / result;
+        }
     }
 };
 
 int main() {
     Solution test;
-    cout << test.myPow(2, -2);
+    cout << test.myPow(1.0000000000001, -2147483648);
     return 0;
 }
