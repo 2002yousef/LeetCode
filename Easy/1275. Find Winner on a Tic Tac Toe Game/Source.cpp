692998a0422dcf992658d56d42d393631918471b
Yousef Wael <yousef.wael2002@gmail.com>
2024-06-29 02:35:53 +0300

    1275. Find Winner on a Tic Tac Toe Game

--- Easy/1275. Find Winner on a Tic Tac Toe Game/Source.cpp
+++ Easy/1275. Find Winner on a Tic Tac Toe Game/Source.cpp
@@ -1,130 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 class Solution {
 public:
     string tictactoe(vector<vector<int>>& moves) {
-
-        vector<char> pos = { '.', '.' , '.' ,'.' ,'.' ,'.' ,'.' ,'.' ,'.' };
+        vector<vector<int>> winningPos = { {0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6} }; // Possible winning positions
+        bool flagA = true;
+        int index;
+        vector<string> positions(9, "");
         for (int i = 0; i < moves.size(); i++)
         {
-            if (i%2 == 0)
+            index = moves[i][0] * 3 + moves[i][1]; // Conversion from 2-d positons to 1-d positions
+            if (flagA)
             {
-                if (moves[i][0] == 0 && moves[i][1] == 0)
-                {
-                    pos[0] = 'A';
-                }
-                else if (moves[i][0] == 0 && moves[i][1] == 1)
-                {
-                    pos[1] = 'A';
-                }
-                else if (moves[i][0] == 0 && moves[i][1] == 2)
-                {
-                    pos[2] = 'A';
-                }
-                else if (moves[i][0] == 1 && moves[i][1] == 0)
-                {
-                    pos[3] = 'A';
-                }
-                else if (moves[i][0] == 1 && moves[i][1] == 1)
-                {
-                    pos[4] = 'A';
-                }
-                else if (moves[i][0] == 1 && moves[i][1] == 2)
-                {
-                    pos[5] = 'A';
-                }
-                else if (moves[i][0] == 2 && moves[i][1] == 0)
-                {
-                    pos[6] = 'A';
-                }
-                else if (moves[i][0] == 2 && moves[i][1] == 1)
-                {
-                    pos[7] = 'A';
-                }
-                else if (moves[i][0] == 2 && moves[i][1] == 2)
-                {
-                    pos[8] = 'A';
-                }
+                positions[index] = "A";
+                flagA = false;
             }
-            else
-            {
-                if (moves[i][0] == 0 && moves[i][1] == 0)
-                {
-                    pos[0] = 'B';
-                }
-                else if (moves[i][0] == 0 && moves[i][1] == 1)
-                {
-                    pos[1] = 'B';
-                }
-                else if (moves[i][0] == 0 && moves[i][1] == 2)
-                {
-                    pos[2] = 'B';
-                }
-                else if (moves[i][0] == 1 && moves[i][1] == 0)
-                {
-                    pos[3] = 'B';
-                }
-                else if (moves[i][0] == 1 && moves[i][1] == 1)
-                {
-                    pos[4] = 'B';
-                }
-                else if (moves[i][0] == 1 && moves[i][1] == 2)
-                {
-                    pos[5] = 'B';
-                }
-                else if (moves[i][0] == 2 && moves[i][1] == 0)
-                {
-                    pos[6] = 'B';
-                }
-                else if (moves[i][0] == 2 && moves[i][1] == 1)
-                {
-                    pos[7] = 'B';
-                }
-                else if (moves[i][0] == 2 && moves[i][1] == 2)
-                {
-                    pos[8] = 'B';
-                }
+            else {
+                positions[index] = "B";
+                flagA = true;
             }
         }
 
-        if ((pos[0] == pos[1] == pos[2]) || (pos[3] == pos[4] == pos[5]) || (pos[6] == pos[7] == pos[8]) || (pos[0] == pos[3] == pos[6]) || (pos[1] == pos[4] == pos[7]) || (pos[2] == pos[5] == pos[8]) || (pos[0] == pos[4] == pos[8]) || (pos[2] == pos[4] == pos[6]))
+        for (int i = 0; i < 8; i++)
         {
-            return "There is a win";
-        }
-        else
-        {
-            return "Pending";
-        }
-    }
-
-       /* vector<vector<int>> xmoves;
-        vector<vector<int>> omoves;
-        for (int i = 0; i < moves.size(); i++)
-        {
-            if (i%2 == 0)
+            if ((positions[winningPos[i][0]] != "" && positions[winningPos[i][1]] != "" && positions[winningPos[i][2]] != "")
+                && (positions[winningPos[i][0]] == positions[winningPos[i][1]]) && (positions[winningPos[i][0]] == positions[winningPos[i][2]]))
             {
-                xmoves.push_back(moves[i]);
-            }
-            else
-            {
-                omoves.push_back(moves[i]);
-            }
-        }
-        if (xmoves.size() == 3)
-        {
-            if (horizontalCheck(xmoves) || verticalCheck(xmoves) || diagonalCheck(xmoves) || reverseDiagonalCheck(xmoves))
-            {
-                return "A";
-            }
-        }
-
-        if (omoves.size() == 3)
-        {
-            if (horizontalCheck(omoves) || verticalCheck(omoves) || diagonalCheck(omoves) || reverseDiagonalCheck(omoves) )
-            {
-                return "B";
+                return positions[winningPos[i][0]];
             }
         }
 
@@ -135,48 +40,12 @@ public:
         else {
             return "Pending";
         }
-
     }
-    bool horizontalCheck(vector<vector<int>> move) {
-        if (move[0][0] == move[1][0] && move[0][0] == move[2][0])
-        {
-            return true;
-        }
-        return false;
-    }
-
-    bool verticalCheck(vector<vector<int>> move) {
-        if (move[0][1] == move[1][1] && move[0][1] == move[2][1])
-        {
-            return true;
-        }
-        return false;
-    }
-
-    bool diagonalCheck(vector<vector<int>> move) {
-        if ((move[0][0] == move[0][1]) && (move[1][0] == move[1][1]) && (move[2][0] == move[2][1]))
-        {
-            return true;
-        }
-        return false;
-    }
-
-    bool reverseDiagonalCheck(vector<vector<int>> move) {
-        if ((move[0][0] + move[0][1] == 2) && (move[1][0] + move[1][1] == 2) && (move[2][0] + move[2][1] == 2))
-        {
-            return true;
-        }
-        return false;
-    }*/
-
-    
 };
 
 int main() {
-    Solution test1;
-    vector<vector<int>> arr1 = { {0,0}, {1,1}, {0,1}, {0,2},  {1,0}, {2,0} };
-    string result1 = test1.tictactoe(arr1);
-    cout << result1;
-
+    Solution test;
+    vector<vector<int>> moves = { {0,0}, {2,0}, {1,1}, {2,1}, {2,2} };
+    cout << test.tictactoe(moves);
     return 0;
 }
